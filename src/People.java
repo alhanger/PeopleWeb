f7a5520c7d89e6b0dfe92fff9f21478a7505aa59
alhanger <hanger129@gmail.com>
2015-10-28 16:25:42 -0400

    Add functionality

--- src/People.java
+++ src/People.java
@@ -1,6 +1,11 @@
+import spark.ModelAndView;
+import spark.Spark;
+import spark.template.mustache.MustacheTemplateEngine;
+
 import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by zach on 10/19/15.
@@ -21,7 +26,50 @@ public class People {
             people.add(person);
         }
 
-        // write Spark route here
+        Spark.get(
+                "/",
+                ((request, response) -> {
+                    String counter = request.queryParams("counter");
+                    int countNum = 0;
+
+                    if (counter == null) {
+                        countNum = 0;
+                    }
+                    else if (counter != null) {
+                        countNum = Integer.valueOf(counter);
+                    }
+                    if (countNum >= people.size() || countNum < 0) {
+                        Spark.halt(403);
+                    }
+
+                    ArrayList<Person> list = new ArrayList<Person>(people.subList(countNum, countNum + 20));
+
+                    HashMap m = new HashMap();
+                    m.put("list", list);
+                    m.put("prevCounter", countNum - 20);
+                    m.put("counter", countNum + 20);
+
+                    return new ModelAndView(m, "people.html");
+                }),
+                new MustacheTemplateEngine()
+        );
+        Spark.get(
+                "/person",
+                ((request, response) -> {
+                    HashMap p = new HashMap();
+                    String id = request.queryParams("id");
+
+                    try{
+                        int idNum = Integer.valueOf(id);
+                        Person person = people.get(idNum - 1);
+                        p.put("person", person);
+                    } catch (Exception e) {
+
+                    }
+                    return new ModelAndView(p, "person.html");
+                }),
+                new MustacheTemplateEngine()
+        );
     }
 
     static String readFile(String fileName) {
